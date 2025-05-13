0f53bf2a4fc2f893ca0c3855a6d906c3bd80d68e
Rahul Ch <xavi3441@gmail.com>
2025-05-13 23:29:10 +0530

    Modified backend, updated with new optimizer

--- backend.py
+++ backend.py
@@ -66,7 +66,7 @@ y=df["sentiment"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
-model = LogisticRegression(max_iter=1000, solver='liblinear', penalty='l1')
+model = LogisticRegression(max_iter=1000, solver='saga', penalty='l1')
 model.fit(X_train, y_train)
 
 y_pred=model.predict(X_test)
